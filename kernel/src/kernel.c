49247b616fc46155325246f049dd652b4931bdde
gourmet <insogna_@outlook.it>
2025-10-23 22:01:03 +0200

    Remove useless msg

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -4,8 +4,7 @@
 void kernel_main() {
 
     idt_init();
-    print("Welcome to your personal os!\n", COLOR_WHITE, COLOR_BLACK);
-    print("What can i do for you\n", COLOR_WHITE, COLOR_BLACK);
+    print("Welcome to your personal os!\n");
 
     for(;;);
 
