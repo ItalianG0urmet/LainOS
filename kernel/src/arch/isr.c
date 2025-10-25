8de785fd7049d25750607c9611c7fe05a391e93e
gourmet <insogna_@outlook.it>
2025-10-25 11:19:27 +0200

    Update isr message

--- kernel/src/arch/isr.c
+++ kernel/src/arch/isr.c
@@ -1,13 +1,9 @@
 #include "arch/isr.h"
-#include "core/format.h"
 #include "core/print.h"
 
-
 void __attribute__((cdecl)) i686_ISR_Handler(struct regs* regs) {
-    char buffer[16];
-    int_to_ascii(regs->int_no, buffer);
-    print(buffer);
 
+    print("isr: %d\n", regs->int_no);
     for (;;);
 }
 
