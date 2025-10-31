4ed4ffd7a6ed5b0bcaeae74d329da2ccd851bb82
gourmet <insogna_@outlook.it>
2025-10-31 15:29:52 +0100

    Add 32bit and 16bit mode

--- kernel/include/core/memory.h
+++ kernel/include/core/memory.h
@@ -3,5 +3,7 @@
 
 #include <stdint.h>
 void* memset(void* ptr, uint8_t value, uint32_t count);
+void* memset16(void* ptr, uint16_t value, uint32_t count);
+void* memset32(void* ptr, uint32_t value, uint32_t count);
 
 #endif
--- kernel/src/core/memory.c
+++ kernel/src/core/memory.c
@@ -7,3 +7,19 @@ void* memset(void* ptr, uint8_t value, uint32_t count){
     }
     return ptr;
 }
+
+void* memset16(void* ptr, uint16_t value, uint32_t count){
+    uint16_t* p = (uint16_t*)ptr;
+    for(uint32_t i = 0; i < count; i++){
+        p[i] = value;
+    }
+    return ptr;
+}
+
+void* memset32(void* ptr, uint32_t value, uint32_t count){
+    uint32_t* p = (uint32_t*)ptr;
+    for(uint32_t i = 0; i < count; i++){
+        p[i] = value;
+    }
+    return ptr;
+}
