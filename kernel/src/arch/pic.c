d12ca5327957adb1326e95ad1354d7a622bf0264
ItalianG0urmet <insogna_@outlook.it>
2025-11-06 12:07:56 +0100

    Add pic implementation

---
+++ kernel/src/arch/pic.c
@@ -0,0 +1,46 @@
+#include "arch/pic.h"
+#include <stdint.h>
+#include "arch/io.h"
+
+#define PIC_MASTER_COMMAND 0x20
+#define PIC_MASTER_DATA 0x21
+#define PIC_SLAVE_COMMAND 0xA0
+#define PIC_SLAVE_DATA 0xA1
+
+#define INIT_AND_WAIT_ICW4 0x11
+
+#define OFFSET_MASTER 0x20
+#define OFFSET_SLAVE 0x28
+
+void static pic_remap(){
+    // Save the mask
+    uint8_t mask1 = inb(PIC_MASTER_DATA);
+    uint8_t mask2 = inb(PIC_SLAVE_DATA);
+
+    // Send ICW1
+    outb(PIC_MASTER_COMMAND, INIT_AND_WAIT_ICW4);
+    outb(PIC_SLAVE_COMMAND, INIT_AND_WAIT_ICW4);
+
+    // Send ICW2
+    outb(PIC_MASTER_DATA, OFFSET_MASTER);
+    outb(PIC_SLAVE_DATA, OFFSET_SLAVE);
+
+    // Send ICW3
+    outb(PIC_MASTER_DATA, 0x04);
+    outb(PIC_SLAVE_DATA, 0x02);
+
+    // Send ICW4 ( set 8080 mode)
+    outb(PIC_MASTER_DATA, 0x01);
+    outb(PIC_SLAVE_DATA, 0x01);
+
+    // Restore mask
+    outb(PIC_MASTER_DATA, mask1);
+    outb(PIC_SLAVE_DATA, mask2);
+
+}
+
+void pic_init(){
+    pic_remap();
+}
+
+
