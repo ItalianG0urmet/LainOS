5ee66695c5dbb8c7d34c2e28747691825d209d2f
ItalianG0urmet <insogna_@outlook.it>
2025-11-06 12:08:27 +0100

    Fix registers order

--- kernel/include/arch/registers.h
+++ kernel/include/arch/registers.h
@@ -2,11 +2,45 @@
 #define REGISTERS_H
 
 #include <stdint.h>
+
+// Order:
+//   pusha        < eax ecx ... edi
+//   mov eax, ds
+//   push eax
+//   mov eax, es
+//   push eax
+//   mov eax, fs
+//   push eax
+//   mov eax, gs
+//   push eax
+//
+//   mov eax, esp
+//   push eax
+
 typedef struct regs {
+    uint32_t gs;
+    uint32_t fs;
+    uint32_t es;
     uint32_t ds;
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    uint32_t int_no, err_code;
-    uint32_t eip, cs, eflags, useresp, ss;
+
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+
+    uint32_t int_no;
+    uint32_t err_code;
+
+    uint32_t eip;
+    uint32_t cs;
+    uint32_t eflags;
+    uint32_t useresp;
+    uint32_t ss;
 } __attribute__((packed)) regs_t;
 
 #endif
+
