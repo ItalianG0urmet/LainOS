e7784d6a7fafaa8619130b1bda65b1d06da026b4
ItalianG0urmet <insogna_@outlook.it>
2025-11-06 12:08:10 +0100

    Call pic

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,8 +1,12 @@
 #include "arch/interrupts/idt.h"
+#include "arch/io.h"
+#include "arch/pic.h"
 #include "core/print.h"
 
 void kernel_main() {
     idt_init();
+    pic_init();
+    sti();
 
     clear_screen();
     print("Welcome to your personal os!\n");
