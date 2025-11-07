188b35a45def79d5de6d5807123888d9ac8e5e97
gourmet <insogna_@outlook.it>
2025-11-07 16:22:53 +0100

    Add test function for keyboard managment

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,15 +2,20 @@
 #include "arch/io.h"
 #include "arch/pic.h"
 #include "core/print.h"
+#include "drivers/keyboard.h"
 
 void kernel_main() {
     idt_init();
     pic_init();
     sti();
-
+    
     clear_screen();
     print("Welcome to your personal os!\n");
-    for (;;);
+    // Test 
+    for (;;){
+        char c = getch();
+        print("%c", c);
+    }
 
     return;
 }
