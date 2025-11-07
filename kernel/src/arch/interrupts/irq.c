c003cafad336a991b09ce89131b37fe1d65cfdbb
gourmet <insogna_@outlook.it>
2025-11-07 16:21:18 +0100

    Add keyboard interrupt managment

--- kernel/src/arch/interrupts/irq.c
+++ kernel/src/arch/interrupts/irq.c
@@ -1,26 +1,54 @@
 #include "arch/interrupts/irq.h"
 #include "arch/io.h"
-#include "core/print.h"
+#include "drivers/keyboard.h"
 #include <stdint.h>
 
+void default_irq_handler(struct regs* regs){
+    (void)regs;
+}
+
+#define DECLARE_IRQ_WEAK(n) \
+void irq##n##_man(struct regs *r) __attribute__((weak, alias("default_irq_handler")))
+
+DECLARE_IRQ_WEAK(0);
+DECLARE_IRQ_WEAK(2);
+DECLARE_IRQ_WEAK(3);
+DECLARE_IRQ_WEAK(4);
+DECLARE_IRQ_WEAK(5);
+DECLARE_IRQ_WEAK(6);
+DECLARE_IRQ_WEAK(7);
+DECLARE_IRQ_WEAK(8);
+DECLARE_IRQ_WEAK(9);
+DECLARE_IRQ_WEAK(10);
+DECLARE_IRQ_WEAK(11);
+DECLARE_IRQ_WEAK(12);
+DECLARE_IRQ_WEAK(13);
+DECLARE_IRQ_WEAK(14);
+DECLARE_IRQ_WEAK(15);
+
+void irq1_man(struct regs *r){
+    keyboard_interrupt_handler();
+}
+
+void (*irq_handlers[16])(struct regs*) = {
+    irq0_man,  irq1_man,  irq2_man,  irq3_man,
+    irq4_man,  irq5_man,  irq6_man,  irq7_man,
+    irq8_man,  irq9_man,  irq10_man, irq11_man,
+    irq12_man, irq13_man, irq14_man, irq15_man
+};
+
+static inline void send_eoi(uint8_t irq_no) {
+    if (irq_no >= 8) {
+        outb(0xA0, 0x20);
+    }
+    outb(0x20, 0x20);
+}
+
 void __attribute__((cdecl)) i686_IRQ_Handler(struct regs* regs){
-    uint8_t irq = (uint8_t) regs->int_no;
+    uint32_t irq = (uint32_t) regs->int_no;
     if (irq >= 0x20) irq -= 0x20;
 
-    switch(irq){
-        case 0: {
-            //Todo: implement
-            break;
-        }
-        case 1: {
-            uint8_t sc = inb(0x60);
-            print("kbd scancode %h\n", sc);
-            //Todo: impement
-            break;
-        }
-        default: {
-            print("Irq %d\n", irq);
-        }
-    }
+    irq_handlers[irq](regs);
+    send_eoi(irq);
 }
 
--- kernel/src/drivers/keyboard.c
+++ kernel/src/drivers/keyboard.c
@@ -1,157 +1,105 @@
 #include "drivers/keyboard.h"
 #include "arch/io.h"
+#include <stdint.h>
 
-static const enum key scancode_map[256] = {
-    [0x01] = KEY_ESC,
-    [0x02] = KEY_1,
-    [0x03] = KEY_2,
-    [0x04] = KEY_3,
-    [0x05] = KEY_4,
-    [0x06] = KEY_5,
-    [0x07] = KEY_6,
-    [0x08] = KEY_7,
-    [0x09] = KEY_8,
-    [0x0A] = KEY_9,
-    [0x0B] = KEY_0,
-    [0x0C] = KEY_MINUS,
-    [0x0D] = KEY_EQUAL,
-    [0x0E] = KEY_BACKSPACE,
-    [0x0F] = KEY_TAB,
-    [0x10] = KEY_Q,
-    [0x11] = KEY_W,
-    [0x12] = KEY_E,
-    [0x13] = KEY_R,
-    [0x14] = KEY_T,
-    [0x15] = KEY_Y,
-    [0x16] = KEY_U,
-    [0x17] = KEY_I,
-    [0x18] = KEY_O,
-    [0x19] = KEY_P,
-    [0x1A] = KEY_LEFTBRACKET,
-    [0x1B] = KEY_RIGHTBRACKET,
-    [0x1C] = KEY_ENTER,
-    [0x1D] = KEY_LEFTCTRL,
-    [0x1E] = KEY_A,
-    [0x1F] = KEY_S,
-    [0x20] = KEY_D,
-    [0x21] = KEY_F,
-    [0x22] = KEY_G,
-    [0x23] = KEY_H,
-    [0x24] = KEY_J,
-    [0x25] = KEY_K,
-    [0x26] = KEY_L,
-    [0x27] = KEY_SEMICOLON,
-    [0x28] = KEY_APOSTROPHE,
-    [0x29] = KEY_GRAVE,
-    [0x2A] = KEY_LEFTSHIFT,
-    [0x2B] = KEY_BACKSLASH,
-    [0x2C] = KEY_Z,
-    [0x2D] = KEY_X,
-    [0x2E] = KEY_C,
-    [0x2F] = KEY_V,
-    [0x30] = KEY_B,
-    [0x31] = KEY_N,
-    [0x32] = KEY_M,
-    [0x33] = KEY_COMMA,
-    [0x34] = KEY_DOT,
-    [0x35] = KEY_SLASH,
-    [0x36] = KEY_RIGHTSHIFT,
-    [0x37] = KEY_KP_ASTERISK,
-    [0x38] = KEY_LEFTALT,
-    [0x39] = KEY_SPACE,
-    [0x3A] = KEY_CAPSLOCK,
-    [0x3B] = KEY_F1,
-    [0x3C] = KEY_F2,
-    [0x3D] = KEY_F3,
-    [0x3E] = KEY_F4,
-    [0x3F] = KEY_F5,
-    [0x40] = KEY_F6,
-    [0x41] = KEY_F7,
-    [0x42] = KEY_F8,
-    [0x43] = KEY_F9,
-    [0x44] = KEY_F10,
-    [0x45] = KEY_NUMLOCK,
-    [0x46] = KEY_SCROLLLOCK,
-    [0x47] = KEY_HOME,
-    [0x48] = KEY_UP,
-    [0x49] = KEY_PGUP,
-    [0x4B] = KEY_LEFT,
-    [0x4D] = KEY_RIGHT,
-    [0x4F] = KEY_END,
-    [0x50] = KEY_DOWN,
-    [0x51] = KEY_PGDN,
-    [0x52] = KEY_INSERT,
-    [0x53] = KEY_DELETE,
-    [0x57] = KEY_F11,
-    [0x58] = KEY_F12
+/* Modificators */
+#define SC_LSHIFT 0x2A
+#define SC_RSHIFT 0x36
+#define SC_CTRL   0x1D
+#define SC_ALT    0x38
+#define SC_CAPS   0x3A
+
+static volatile int shift_down = 0;
+static volatile int ctrl_down  = 0;
+
+/* Ring */
+#define KEYBOARD_BUFFER_LIMIT 255
+
+static volatile uint16_t kbd_keys[KEYBOARD_BUFFER_LIMIT];
+static volatile uint16_t kbd_head = 0; // Next slot to write
+static volatile uint16_t kbd_tail = 0; // Next slot to read
+
+/* Conversion */
+static const char scancode_to_ascii[128] = {
+    0,  27, '1','2','3','4','5','6',   /* 0x00 - 0x07 */
+    '7','8','9','0','-','=','\b','\t',  /* 0x08 - 0x0F */
+    'q','w','e','r','t','y','u','i',   /* 0x10 - 0x17 */
+    'o','p','[',']','\n', 0,  'a','s',  /* 0x18 - 0x1F */
+    'd','f','g','h','j','k','l',';',   /* 0x20 - 0x27 */
+    '\'', '`', 0,  '\\','z','x','c','v',/* 0x28 - 0x2F */
+    'b','n','m',',','.','/', 0,   '*',  /* 0x30 - 0x37 */
+    0,  ' ', 0,   0,   0,   0,   0,   0, /* 0x38 - 0x3F */
 };
 
-enum key get_equ_key(uint8_t scancode) {
-    if (scancode >= sizeof(scancode_map))
-        return KEY_UNKNOWN;
-    enum key k = scancode_map[scancode];
-    return k ? k : KEY_UNKNOWN;
-}
-
-uint8_t get_last_scancode() {
-    uint8_t status = inb(0x64);
-    if (!(status & 0x01))
-        return 0;
-
-    uint8_t scancode = inb(0x60);
-    if (scancode & 0x80)
-        return 0;
-
-    return scancode;
-}
-
-static const char key_ascii_map[256] = {
-    [0x02] = '1',
-    [0x03] = '2',
-    [0x04] = '3',
-    [0x05] = '4',
-    [0x06] = '5',
-    [0x07] = '6',
-    [0x08] = '7',
-    [0x09] = '8',
-    [0x0A] = '9',
-    [0x0B] = '0',
-    [0x10] = 'q',
-    [0x11] = 'w',
-    [0x12] = 'e',
-    [0x13] = 'r',
-    [0x14] = 't',
-    [0x15] = 'y',
-    [0x16] = 'u',
-    [0x17] = 'i',
-    [0x18] = 'o',
-    [0x19] = 'p',
-    [0x1E] = 'a',
-    [0x1F] = 's',
-    [0x20] = 'd',
-    [0x21] = 'f',
-    [0x22] = 'g',
-    [0x23] = 'h',
-    [0x24] = 'j',
-    [0x25] = 'k',
-    [0x26] = 'l',
-    [0x2C] = 'z',
-    [0x2D] = 'x',
-    [0x2E] = 'c',
-    [0x2F] = 'v',
-    [0x30] = 'b',
-    [0x31] = 'n',
-    [0x32] = 'm',
-    [0x39] = ' ',
-    [0x0F] = '\t',
-    [0x1C] = '\n',
-    [0x0E] = '\b', 
+static const char scancode_to_ascii_shift[128] = {
+    0,  27, '!', '@','#','$','%','^',
+    '&','*','(',')','_','+','\b','\t',
+    'Q','W','E','R','T','Y','U','I',
+    'O','P','{','}','\n', 0,  'A','S',
+    'D','F','G','H','J','K','L',':',
+    '"', '~', 0,  '|','Z','X','C','V',
+    'B','N','M','<','>','?', 0,   '*',
+    0,  ' ', 0,   0,   0,   0,   0,   0,
 };
 
-char key_ascii(uint8_t scancode) {
-    if (scancode >= sizeof(key_ascii_map))
-       return 0;
-    char c = key_ascii_map[scancode];
-    return c ? c : 0; //Todo: fix this always return null
+/* Ring Implementation */
+static inline int kbd_is_empty(void) {
+    return kbd_head == kbd_tail;
 }
 
+static void kbd_buffer_push(uint16_t char_code){
+    uint16_t next = (kbd_head + 1) % KEYBOARD_BUFFER_LIMIT;
+    if (next == kbd_tail) return; // Ignore
+    
+    kbd_keys[kbd_head] = char_code;
+    kbd_head = next;
+}
+
+static uint16_t kbd_buffer_pop(){
+    uint16_t ret_val = -1;
+    cli();
+    if(!kbd_is_empty()){
+        ret_val = (uint16_t)kbd_keys[kbd_tail];
+        kbd_tail = (kbd_tail + 1) % KEYBOARD_BUFFER_LIMIT;
+    }
+    sti();
+    return ret_val; // return -1 if empty
+}
+
+char getch(){
+    char c;
+    for (;;){
+        c = kbd_buffer_pop();
+        if(c != -1) return c;
+        sti();
+        hlt();
+    }
+}
+
+void keyboard_interrupt_handler(){
+
+    uint8_t sc = inb(0x60);
+    uint8_t key = sc & 0x7F;
+    uint8_t released = sc & 0x80;
+
+    if (key == SC_LSHIFT || key == SC_RSHIFT) {
+        if (released) shift_down = 0;
+        else shift_down = 1;
+        return;
+    }
+
+    if(released){
+        return;
+    }
+
+    char ch = 0;
+    if (shift_down) {
+        ch = (key < 128) ? scancode_to_ascii_shift[key] : 0;
+    } else {
+        ch = (key < 128) ? scancode_to_ascii[key] : 0;
+    }
+
+    if (ch) {
+        kbd_buffer_push(ch);
+    }
+}
