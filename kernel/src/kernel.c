6a37915fe6fee1c6ec9167d47b7942aafae1b760
ItalianG0urmet <insogna_@outlook.it>
2025-11-11 13:10:13 +0100

    Add infinite loop on shell exit

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -15,6 +15,8 @@ void kernel_main() {
     // Kernel debug shell
     kshell_start();
     
+    printk_color("No programming running, starting loop...", VGA_COLOR_RED, VGA_COLOR_BLACK);
+    for(;;);
 
     return;
 }
