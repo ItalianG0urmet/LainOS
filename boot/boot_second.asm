ade2deb3a89c93863ff4f14fb2a05bde80a138fd
Gourmet <insogna_@outlook.it>
2025-11-23 08:57:53 +0100

    Refactor read_disk in his own file

--- boot/boot_second.asm
+++ boot/boot_second.asm
@@ -25,12 +25,12 @@ second_stage:
     mov si, ascii_9
     call print_string
 
-    call wait_key                   ; Wait for key press
+    call wait_key               ; Wait for key press
 
-    mov dl, [0x7E00]                ; Get the saved boot_sector
+    mov dl, [0x7E00]            ; Get the saved boot_sector
 
-    call read_disk                  ; Read kernel from disk
-    call start_protected            ; Switch to protected mode
+    call read_disk              ; Read kernel from disk
+    call start_protected        ; Switch to protected mode
 
 ;----------------
 ; Data
@@ -46,38 +46,16 @@ ascii_7             db "|      Press a key to continue    |",0Dh,0Ah,0
 ascii_8             db "|                                 |",0Dh,0Ah,0
 ascii_9             db "+---------------------------------+",0Dh,0Ah,0
 
-CODE_SEG            equ gdt_code - gdt_start                   ; Offset of code segment in GDT
-DATA_SEG            equ gdt_data - gdt_start                   ; Offset of data segment in GDT
-KERNEL_LOCATION     equ 0x2000                                 ; Load address of kernel
-KERNEL_SECTORS      equ 13                                     ; Number of sectors to read
-KERNEL_START_SECTOR equ 4                                      ; Start sector on disk
+CODE_SEG            equ gdt_code - gdt_start   ; Offset of code segment in GDT
+DATA_SEG            equ gdt_data - gdt_start   ; Offset of data segment in GDT
+KERNEL_LOCATION     equ 0x2000                 ; Load address of kernel
+KERNEL_SECTORS      equ 13                     ; Number of sectors to read
+KERNEL_START_SECTOR equ 4                      ; Start sector on disk
 
 %include "boot/graphics.asm" ; Include (copy) print_string, clear_screen and wait_key
-
-;----------------
-; Disk operations
-read_disk:
-    cli
-    xor ax, ax
-    mov ds, ax                  ; Set DS to 0
-    mov es, ax                  ; Set ES to 0
-    mov bx, KERNEL_LOCATION     ; Destination address for disk read
-
-    mov ah, 0x02                ; BIOS read sectors function
-    mov al, KERNEL_SECTORS      ; Number of sectors to read
-    mov ch, 0x00                ; Cylinder 0
-    mov cl, KERNEL_START_SECTOR ; Start sector
-    mov dh, 0x00                ; Head 0
-    int 0x13                    ; BIOS disk interrupt
-    jc disk_error               ; Jump if carry flag set (error)
-    ret                         ; Return if success
-
-disk_error:
-    mov si, msg_disk_error      ; Load error message
-    call print_string           ; Print error
-    hlt                         ; Halt CPU
-    jmp $                       ; Infinite loop
-
+%include "boot/disk.asm"     ; Include 'read_disk', require:
+                             ; msg_disk_error, KERNEL_SECTOR, KERNEL_START_SECTOR
+                             ; and KERNEL_LOCATION
 ;----------------
 ; Protected mode setup
 start_protected:
