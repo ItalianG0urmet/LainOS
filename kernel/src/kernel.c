b08f2a564737b3027810f2ddf6326179002e6000
Gourmet <insogna_@outlook.it>
2025-12-21 09:37:15 +0100

    Replace for(;;) with panick()

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -3,30 +3,23 @@
 #include "arch/pic.h"
 #include "core/print.h"
 #include "core/boot_info.h"
+#include "core/panic.h"
 #include "utils/kshell/kshell.h"
 
 #define BOOT_IDENTIFIER 0x1BADB002
 
 void kernel_main(u32 magic, u32 boot_info)
 {
-
     boot_info_init(boot_info);
 
-    if(magic != BOOT_IDENTIFIER) {
-        clear_screenk();
-        printk_color("Wrong bootloader identifier, starting loop...\n", VGA_COLOR_RED, VGA_COLOR_BLACK);
-        for(;;);
-    }
+    if(magic != BOOT_IDENTIFIER)
+        panick("Wrong bootloader identifier");
 
     idt_init();
     pic_init();
     sti();
 
-
     kshell_start();
 
-    printk_color("No programming running, starting loop...", VGA_COLOR_RED, VGA_COLOR_BLACK);
-    for(;;);
-
-    return;
+    panick("No programming running");
 }
