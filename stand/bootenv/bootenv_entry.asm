6c33f2c39a66adb0f53f5a73fac8e64634a2e834
frapank <insogna_@outlook.it>
2026-04-16 21:16:03 +0200

    Fix jmp to bootenv

--- stand/bootenv/bootenv_entry.asm
+++ stand/bootenv/bootenv_entry.asm
@@ -1,8 +1,18 @@
 [bits 32]
+jmp start
 
+section .entry
+global start
 extern bootenv_entry
 
-section .text
-_start:
+start:
+    cli
+
     call bootenv_entry
 
+.halt:
+    hlt
+    jmp .halt
+
+
+times 8192 - ($-$$) db 0
