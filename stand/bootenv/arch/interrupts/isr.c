debb052f3c88be6556324e3d0e1204148081f2b0
frapank <insogna_@outlook.it>
2026-04-22 09:59:40 +0200

    Add panic file

--- stand/bootenv/arch/interrupts/isr.c
+++ stand/bootenv/arch/interrupts/isr.c
@@ -1,6 +1,7 @@
 #include "arch/interrupts/isr.h"
 #include "utils/defhelp.h"
 #include "utils/types.h"
+#include "core/panic.h"
 
 static char* const exception_names[32] = {
     "0: Division for zero",
@@ -86,7 +87,7 @@ static void (* const isr_handlers[32])(struct regs*) = {
 
 static void __attribute__((noreturn)) default_isr_handler(struct regs* regs)
 {
-    //panick(exception_names[regs->int_no]);
+    panick(exception_names[regs->int_no]);
     for(;;);
 }
 
@@ -97,7 +98,7 @@ void __attribute__((cdecl)) i686_ISR_Handler(struct regs* regs)
     if (likely(num < 32)) {
         isr_handlers[num](regs);
     } else {
-        //panick("Invalid interrupt number\n");
+        panick("Invalid interrupt number\n");
     }
 }
 
