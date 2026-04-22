ce26df59315d1a12c6f78385d871ad92efbdf70f
frapank <insogna_@outlook.it>
2026-04-22 10:04:49 +0200

    Add test message

--- stand/bootenv/bootenv.c
+++ stand/bootenv/bootenv.c
@@ -9,6 +9,5 @@ void bootenv_entry(void)
     pic_init();
 
     clear_screenk();
-    vga_put_char('H', VGA_COLOR_BLACK, VGA_COLOR_RED, 1, 1);
-    vga_put_char('i', VGA_COLOR_BLACK, VGA_COLOR_RED, 2, 1);
+    printk("Hello world");
 }
