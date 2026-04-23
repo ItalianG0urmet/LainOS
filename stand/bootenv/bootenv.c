fadaa4746393dc001c5f27295ce33ef9d517196a
frapank <insogna_@outlook.it>
2026-04-23 23:06:04 +0200

    Add welcome message to new bootloader

--- stand/bootenv/bootenv.c
+++ stand/bootenv/bootenv.c
@@ -1,13 +1,27 @@
 #include "arch/interrupts/idt.h"
 #include "arch/pic.h"
-#include "drivers/vga_text.h"
 #include "core/print_vga_text.h"
+#include "drivers/vga_text.h"
+#include "drivers/keyboard.h"
+
+char* wlc_message =
+"+----------------------------+\n"
+"|                            |\n"
+"|       L A I N   O S        |\n"
+"|                            |\n"
+"|   experimental bootloader  |\n"
+"|                            |\n"
+"+----------------------------+";
 
 void bootenv_entry(void)
 {
+    // Init
     idt_init();
     pic_init();
 
+    // User
     clear_screenk();
-    printk("Hello world");
+    printk(wlc_message);
+
+    // Finish
 }
